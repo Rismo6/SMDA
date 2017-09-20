635a8293f9e333155818c8d3038db0b00b871f70
Rismo6 <32010768+Rismo6@users.noreply.github.com>
2017-09-19 20:53:54 -0700

    Add files via upload

---
+++ Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        
+        
+           
+            // Read the jump input in Update so button presses aren't missed.
+           
+        
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+           ;
+            // Pass all parameters to the character control script.
+           
+            jump = false;
+        }
+    }
+}
